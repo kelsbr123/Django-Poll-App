8b1c7b98233e078c6ba012dbb9b875b6fc6c3e9c
kelsb <kelsbr123@gmail.com>
2024-03-04 20:40:13 -0700

    Updated urls

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime as dt
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -10,9 +11,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - dt.timedelta(days=1)
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - dt.timedelta(days=1) <= self.pub_date <= now
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
